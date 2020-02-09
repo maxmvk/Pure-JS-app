535557753a8c48e8a9ffdb21abd9eff0a0a4730c
maxmvk <maxmvk1993@gmail.com>
2020-02-09 23:56:29 +0200

    Commit organizer

---
+++ Angular-organizer/src/app/selector/selector.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { DateService } from './shared/data.service';
+
+@Component({
+  selector: 'app-selector',
+  templateUrl: './selector.component.html',
+  styleUrls: ['./selector.component.css']
+})
+export class SelectorComponent {
+
+  constructor(private dateService: DateService) { }
+
+  go(dir: number) {
+    this.dateService.changeMonth(dir)
+  }
+
+}
